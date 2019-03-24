46f29ac2f350583a4044332bef64555d6407e86a
Odinei Ramone <odineiramone@gmail.com>
2019-03-23 23:35:14 -0300

    Connect to Postgresql
      - Dotenv gem to get environment variables
        from a '.env' file
    
      - Pg gem to connect to PostgreSQL
    
      - Updated activerecord gem to work with
        pg gem
    
      - Database configuration files

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,11 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
+require 'dotenv/load'
+
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+template = ERB.new(File.new('./config/database.yml').read)
+db_yaml = YAML.load(template.result(binding))
+ActiveRecord::Base.establish_connection(db_yaml[ENV['SINATRA_ENV']])
 
 require './app/controllers/application_controller'
 require_all 'app'
