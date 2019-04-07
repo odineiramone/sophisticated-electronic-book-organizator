e0fa35fa399451f76cb566b3969ddfb2ce8cb93a
Odinei Ribeiro <odinei.ribeiro@plataformatec.com.br>
2019-04-06 22:10:14 -0300

    Validations to books (#1)
    Add validation on book title. Validation errors is showed on flash message.
    
    Other improvements:
    
    - Fixed ruby version on 2.6.2
    - Add sinatra flash messages gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::Flash
 
   configure do
+    set :sessions, true
+    set :session_secret, ENV['SESSION_SECRET']
     set :public_folder, 'public'
     set :views, 'app/views'
   end
@@ -10,5 +13,4 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :welcome
   end
-
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,44 +1,64 @@
 class BooksController < ApplicationController
+  # INDEX
   get "/books" do
     @books = Book.all
     erb :"/books/index.html"
   end
 
+  # NEW
   get "/books/new" do
+    @book = Book.new params[:book]
     erb :"/books/new.html"
   end
 
+  # CREATE
   post "/books" do
     @book = Book.new params[:book]
 
-    if @book.save
+    if @book.valid?
+      @book.save
+
+      flash[:success] = 'Livro alterado com sucesso!'
       redirect "/books"
     else
-      erb :"/books/new.html"
+      flash[:error] = 'O Livro não pode ser criado :('
+
+      query = { book: @book.attributes }.to_query
+      redirect "/books/new?#{query}"
     end
   end
 
+  # SHOW
   get "/books/:id" do
     @book = Book.find_by id: params["id"]
     erb :"/books/show.html"
   end
 
+  # EDIT
   get "/books/:id/edit" do
     @book = Book.find_by id: params["id"]
     erb :"/books/edit.html"
   end
 
+  # UPDATE
   patch "/books/:id" do
     @book = Book.find_by id: params["id"]
     @book.assign_attributes params['book']
 
-    if @book.save
+    if @book.valid?
+      @book.save
+
+      flash[:success] = 'Livro alterado com sucesso!'
       redirect "/books/#{@book.id}"
     else
-      erb :"/books/new.html"
+      flash[:error] = 'O Livro não pode ser alterado :('
+
+      query = { book: @book.attributes }.to_query
+      redirect "/books/#{@book.id}/edit?#{query}"
     end
   end
 
+  # DELETE
   delete "/books/:id/delete" do
     @book = Book.find_by id: params["id"]
     @book.destroy
